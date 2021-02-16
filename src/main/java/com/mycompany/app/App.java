cd5264783e2cfa0125079e573703372c41e093f8
sujjadshaik <sujjamohammedshaik@gmail.com>
2021-02-16 16:17:37 +0530

    updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ public class App
 {
 
     private final String message = "Hello World!";
-    //maven app for jenkins testing
+    //maven app for jenkins testing with sonar cude
     public App() {}
 
     public static void main(String[] args) {
